3a3c6b9926b21379f0f35612a2529a98760bf32a
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2026-04-09 12:14:06 -0300

    Variáveis em JS

---
+++ CONTEÚDO/22-VariaveisJS/variaveis.js
@@ -0,0 +1,15 @@
+let nome = 'Elvis' //string
+let sobrenome = 'Bail' //string
+let string = `O ${nome} tem o sobrenome ${sobrenome}` //template string
+
+let idade = 20 //number
+let feliz = true //boolean
+let nada //undefined
+let simbolo = Symbol() //simbolo
+let object = {} //objeto
+
+let mais = 200 + 200 //adição
+let menos = 200 - 200 //subtração
+let vezes = 200 * 200 //multiplicação
+let dividir = 200 / 200 //divisão
+let resto = 200 % 200 //resto da divisão
