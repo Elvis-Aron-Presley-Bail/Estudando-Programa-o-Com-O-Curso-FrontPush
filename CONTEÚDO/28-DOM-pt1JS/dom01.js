a9305e13e21f819661cf406a8ca5eed0af897ec7
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2026-04-15 11:57:44 -0300

    Introdução ao DOM em JS (revendo conceitos)

---
+++ CONTEÚDO/28-DOM-pt1JS/dom01.js
@@ -0,0 +1,6 @@
+let h1 = document.querySelector("h1")
+
+console.log(h1) //Exibe <h1>DOM em JS</h1>
+console.log(h1.innerText) //Exibe DOM em JS
+console.log(h1.classList[1]) //Exibe as classes desse elemento
+console.log(h1.id) //Exibe o id
