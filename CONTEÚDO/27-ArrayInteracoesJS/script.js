ac1fb0076197167eb4689554f2ce7c39c6ef33b0
Elvis Aron Presley Bail <elvisbail6@gmail.com>
2026-04-15 11:57:09 -0300

    Revisando interações com Arrays em JS

--- CONTEÚDO/27-ArrayInteracoesJS/script.js
+++ CONTEÚDO/27-ArrayInteracoesJS/script.js
@@ -1,3 +1,5 @@
+/*
+
 let carros = ['Fiat', 'Audi', 'Ferrari', 'VW']
 let modelos = ['Argo', 'A3', 'La Ferrari', 'Polo']
 
@@ -23,4 +25,40 @@ const modelosCarros = carros.map((elemento, indice) => {
     return elemento + ' ' + modelos[indice]
 })
 
-console.log(modelosCarros)
+console.log(modelosCarros) */
+
+
+let li = document.querySelectorAll('li')
+
+li.forEach((item, posicao) => {
+    item.classList.add(`Item-${posicao+1}`)
+})
+
+
+const numeros = [2, 4, 6, 8, 10]
+
+const numerosX2 = numeros.map(n => n * 2)
+
+console.log(numerosX2)
+
+
+const produtos = [
+    {
+        item: 'Mouse',
+        preco: 30
+    },
+    {
+        item: 'Teclado',
+        preco: 50
+    },
+    {
+        item: 'Memória Ram',
+        preco: 4030
+    }
+]
+
+let valorTotal = 0
+
+produtos.map(elemento => valorTotal += elemento.preco)
+
+console.log(valorTotal)
